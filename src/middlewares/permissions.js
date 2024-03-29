ed781e3b06d1f539224ad1389d0e3d6893c4ef07
ensar <ensarsagkal@gmail.com>
2024-03-29 14:51:17 +0300

    feat: permissions defined

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -14,4 +14,12 @@ module.exports = {
             throw new Error('NoPermission: You must login.')
         }
     },
+    isAdmin:(req,res,next)=>{
+        if (req.user && req.user.isActive&& req.user.isAdmin) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login and Admin.')
+        }
+    }
 }
